56e4ce39f2e430c86b52bb344d84c861d4448400
Vinayak Sharma <vsharma@bayviewglen.ca>
2023-10-10 14:28:03 -0400

    DOTS AND BOXES ACTUAL FINAL

--- Unit One - Foundation of Python Programming/Dots And Boxes Assignment/dotsandboxes.py
+++ Unit One - Foundation of Python Programming/Dots And Boxes Assignment/dotsandboxes.py
@@ -34,14 +34,22 @@ percentage5 = yourpoints5/36
 
 
 
-
 print("Dots and Boxes Score Tracker")
 
 print(f"Player's Name: {name}")
-print("Opponent            Your Points   Opponent Points      Box%")
+print("Opponent            Your Points   Opponent Points      Box(%)")
 print("===========================================================")
-print(f"{opponent1:>8} {yourpoints1:>22} {theirpoints1:>17} {percentage1:>104.2%}") 
-print(f"{opponent2:>8} {yourpoints2:>22} {theirpoints2:>17} {percentage2:>104.2%}") 
-print(f"{opponent3:>8} {yourpoints3:>22} {theirpoints3:>17} {percentage3:>104.2%}") 
-print(f"{opponent4:>8} {yourpoints4:>22} {theirpoints4:>17} {percentage4:>104.2%}") 
-print(f"{opponent5:>8} {yourpoints5:>22} {theirpoints5:>17} {percentage5:>104.2%}") 
+print(f"{opponent1:>8} {yourpoints1:>22} {theirpoints1:>17} {percentage1:>10.2f}") 
+print(f"{opponent2:>8} {yourpoints2:>22} {theirpoints2:>17} {percentage2:>10.2f}") 
+print(f"{opponent3:>8} {yourpoints3:>22} {theirpoints3:>17} {percentage3:>10.2f}") 
+print(f"{opponent4:>8} {yourpoints4:>22} {theirpoints4:>17} {percentage4:>10.2f}") 
+print(f"{opponent5:>8} {yourpoints5:>22} {theirpoints5:>17} {percentage5:>10.2f}") 
+
+tp = yourpoints1 + yourpoints2 + yourpoints3 + yourpoints4 + yourpoints5
+top = theirpoints1 + theirpoints2 + theirpoints3 + theirpoints4 + theirpoints5
+ppr = (percentage1 + percentage2 + percentage3 + percentage4 + percentage5)/5
+
+print("Summary: ")
+print(f"Total Points: {tp}")
+print(f"Total Opponent Points: {top}")
+print(f"Total Opponent Points: {ppr:.2%}")
