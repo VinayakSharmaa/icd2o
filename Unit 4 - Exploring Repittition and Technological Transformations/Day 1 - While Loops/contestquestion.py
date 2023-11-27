ff07f40123fe555bb0801a1dc83806fd3ee286b6
Vinayak Sharma <vsharma@bayviewglen.ca>
2023-11-27 15:02:38 -0500

    pepper

---
+++ Unit 4 - Exploring Repittition and Technological Transformations/Day 1 - While Loops/contestquestion.py
@@ -0,0 +1,22 @@
+num_peppers = int(input("Please enter the amount of peppers: "))
+count = 0
+total = 0
+
+while count < num_peppers: 
+    pepper = input("Please enter the type of pepper: ")
+    if pepper == 'Poblano': 
+        total += 1500
+    elif pepper == 'Mirasol': 
+        total += 6000
+    elif pepper == 'Serrano': 
+        total += 15500
+    elif pepper == 'Cayenne': 
+        total += 40000
+    elif pepper == 'Thai': 
+        total += 75000
+    elif pepper == 'Habanero': 
+        total += 125000
+    count += 1
+
+print(total)
+    
